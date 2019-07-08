c5ce1ebc3fbd4acdb602204fe60d2c06a938cf35
王宏刚 <whg@wanghonggangdeMacBook-Pro.local>
2019-07-08 21:59:36 +0800

    Initial commit

---
+++ python_test/demo2.py
@@ -0,0 +1,16 @@
+#!/ser/bin/env python
+# -*- coding:utf-8 -*-
+
+
+import re
+str = 'aaaaabhghabfb'
+# 匹配a-z里面的元素
+ret = re.findall('[a-z]','acdx')
+print(ret)
+# 结果 ['a', 'c', 'd', 'x']
+
+
+def add():
+    # TOODO 开始完成
+    print('adad')
+
